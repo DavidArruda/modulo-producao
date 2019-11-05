d5fbddfebbc49105a27a54a1ffe2305f850ec9b3
DavidArruda <deh0_@hotmail.com>
2019-11-05 16:01:16 -0200

    Alteracao na Classe BeanOperacao, alteracão no relacionamento entre produto e operação

--- src/beans/BeanOperacao.java
+++ src/beans/BeanOperacao.java
@@ -11,9 +11,7 @@ public class BeanOperacao {
 
 	private Long codOperacao;
 	private Integer nOperacao;
-	private Long ficha_de_preparacao;
-	private Long desenho;
-	private Long produto;
+	private BeanProduto produto;
 
 	public Long getCodOperacao() {
 		return codOperacao;
@@ -31,28 +29,13 @@ public class BeanOperacao {
 		this.nOperacao = nOperacao;
 	}
 
-	public Long getFicha_de_preparacao() {
-		return ficha_de_preparacao;
-	}
-
-	public void setFicha_de_preparacao(Long ficha_de_preparacao) {
-		this.ficha_de_preparacao = ficha_de_preparacao;
-	}
-
-	public Long getDesenho() {
-		return desenho;
-	}
-
-	public void setDesenho(Long desenho) {
-		this.desenho = desenho;
-	}
-
-	public Long getProduto() {
+	public BeanProduto getProduto() {
 		return produto;
 	}
 
-	public void setProduto(Long produto) {
+	public void setProduto(BeanProduto produto) {
 		this.produto = produto;
 	}
+
 
 }
