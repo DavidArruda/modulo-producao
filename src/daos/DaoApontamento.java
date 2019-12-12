48ec9b7c8040da29c50b90cdc4eb0b8f2667b420
DavidArruda <deh0_@hotmail.com>
2019-12-12 16:17:29 -0200

    criação metodo listar e formatar data

--- src/daos/DaoApontamento.java
+++ src/daos/DaoApontamento.java
@@ -3,7 +3,12 @@ package daos;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.List;
 
 import beans.BeanApontamento;
 import connection.SingleConnection;
@@ -28,6 +33,7 @@ public class DaoApontamento {
 			statement.setLong(3, apontamento.getUsuario().getCodUsuario());
 			statement.setLong(4, apontamento.getOperacao().getCodOperacao());
 			statement.setLong(5, apontamento.getOrdem_servico().getCodOs());
+			
 			statement.execute();
 
 			connection.commit();
@@ -42,6 +48,38 @@ public class DaoApontamento {
 				e1.printStackTrace();
 			}
 		}
+	}
+	
+	public List<BeanApontamento> listar() throws SQLException {
+		
+		String sql = "select * from apontamento";
+		
+		List<BeanApontamento> apontamentos = new ArrayList<BeanApontamento>();
+		
+		PreparedStatement statement = connection.prepareStatement(sql);
+		
+		ResultSet resultSet = statement.executeQuery();
+		
+		while(resultSet.next()) {
+			
+			BeanApontamento apontamento = new BeanApontamento();
+			
+			apontamento.setCodApontamento(resultSet.getLong("codApontamento"));
+			apontamento.setInicio(formatarData(resultSet.getDate("inicio")));
+			apontamento.setTermino(formatarData(resultSet.getDate("termino")));
+			apontamento.getUsuario().setCodUsuario(resultSet.getLong("codUsuario"));
+			apontamento.getOperacao().setCodOperacao(resultSet.getLong("operacao"));
+			apontamento.getOrdem_servico().setCodOs(resultSet.getLong("os"));
+		}
+		
+		return apontamentos;
+	}
+	
+	public LocalDateTime formatarData(Date dataDoBanco) {
+
+		LocalDateTime localDateTime = dataDoBanco.toInstant().atZone( ZoneId.systemDefault() ).toLocalDateTime();
+		
+		return localDateTime;
 	}
 
 }
